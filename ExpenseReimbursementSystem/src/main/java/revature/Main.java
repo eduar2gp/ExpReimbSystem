920d2fc9f98f9cbfb784ea1751a75bd2622dd0e0
Eduardo Gonzalez <eduar2gp@gmail.com>
2021-11-09 17:03:24 -0500

    removed testing code in Main class

--- ExpenseReimbursementSystem/src/main/java/revature/Main.java
+++ ExpenseReimbursementSystem/src/main/java/revature/Main.java
@@ -4,38 +4,11 @@ import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
 import revature.controller.endpoints.FrontController;
 
-public class Main {
-	
-	
-	
-	public static class Singleton {		
-		
-		private static Singleton singleton;
-		String name = "Eduar";
-		
-		private Singleton() {			
-		}				
-		
-		public static Singleton getSingleton() {
-			if(singleton ==  null) {
-				singleton = new Singleton();
-			}
-			return singleton;
-		}		
-	}
-	
+public class Main {	
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {				
 		
-		/*Singleton s = Singleton.getSingleton();
-		Singleton s1 = Singleton.getSingleton();
-		System.out.println(s == s1);*/
-		
-		System.out.println(System.getenv("EMAIL_USERNAME"));
-		System.out.println(System.getenv("EMAIL_PASSWORD"));
-		
-		
-		/*Javalin app = Javalin.create(config -> {
+		Javalin app = Javalin.create(config -> {
 			config.addStaticFiles(
 					staticFiles -> {
 						staticFiles.directory="/resources";
@@ -44,6 +17,6 @@ public class Main {
 			});
 		}).start(9001);			
 		
-		new FrontController(app);	*/
+		new FrontController(app);
 	}
 }
